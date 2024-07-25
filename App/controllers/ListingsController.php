9ec3fa8ce03b9e223ee627516fe649e4a9153137
Bewizi <toluwalase.bewaji@yahoo.com>
2024-07-25 12:30:06 +0100

    worked on editing of when a user wants to post a job, updating LisingsController file

--- App/controllers/ListingsController.php
+++ App/controllers/ListingsController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use Framework\Database;
+use Framework\Validation;
 
 class ListingsController
 {
@@ -22,6 +23,8 @@ class ListingsController
 
   public function index()
   {
+    // inspectAndDie(Validation::string(''));
+    // inspectAndDie(Validation::email('text@ext.com'));
 
     $listings = $this->db->query('SELECT * FROM listings')->fetchAll();
 
@@ -44,7 +47,8 @@ class ListingsController
 
   /**
    * Show a single listing
-   *
+   * 
+   *@param array $params
    * @return void
    */
 
@@ -65,6 +69,129 @@ class ListingsController
     }
 
     loadView('listings/show', [
+      'listing' => $listing
+    ]);
+  }
+
+  /**
+   * Store data in databse
+   * 
+   * @return void
+   */
+  public function store()
+  {
+    $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
+
+    $newListingData = array_intersect_key($_POST, array_flip($allowedFields));
+
+    $newListingData['user_id'] = 1;
+
+    $newListingData = array_map('sanitize', $newListingData);
+
+    $requiredFields = ['title', 'description', 'email', 'city', 'state', 'salary'];
+
+    $errors = [];
+
+    foreach ($requiredFields as $field) {
+      if (empty($newListingData[$field]) || !Validation::string($newListingData[$field])) {
+        $errors[$field] = ucfirst($field) . ' is requred';
+      }
+    }
+
+    if (!empty($errors)) {
+      //Reload view with errors
+      loadView('listings/create', [
+        'errors' => $errors,
+        'listing' => $newListingData
+      ]);
+    } else {
+      // Submit data
+      // echo 'Success';
+
+      $fields = [];
+      foreach ($newListingData as $field => $value) {
+        $fields[] = $field;
+      }
+      $fields = implode(', ', $fields);
+
+      $values = [];
+      foreach ($newListingData as $field => $value) {
+        //  Convert empty strings to null
+        if ($value === '') {
+          $newListingData[$field] = null;
+        }
+        $values[] = ':' . $field;
+      }
+
+      $values = implode(', ', $values);
+
+      $query = "INSERT INTO listings({$fields}) VALUES({$values})";
+
+      $this->db->query($query, $newListingData);
+
+      redirect('/listings');
+
+      // inspectAndDie($values);
+    }
+  }
+
+  /**
+   * Delete a listing
+   * 
+   * @param array $params
+   * @return void
+   */
+  public function destory($params)
+  {
+    $id = $params['id'];
+
+    $params = [
+      'id' => $id
+    ];
+
+    $listing = $this->db->query('SELECT*FROM listings WHERE id= :id', $params)->fetch();
+
+    if (!$listing) {
+      ErrorController::notFound('Listing not found');
+      return;
+    }
+    // inspect($listing);
+
+    $this->db->query('DELETE FROM listings WHERE id=:id', $params);
+
+    //Set flash message
+    $_SESSION['success_message'] = 'Listing deleted successfully';
+
+    redirect('/listings');
+  }
+
+
+  /**
+   * Show the listing edit form
+   * 
+   *@param array $params
+   * @return void
+   */
+
+  public function edit($params)
+  {
+    $id = $params['id'] ?? '';
+
+    $params = [
+      'id' => $id
+    ];
+
+    $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    // Check if lisings exists
+    if (!$listing) {
+      ErrorController::notFound('Lising not found');
+      return;
+    }
+
+    // inspectAndDie($listing);
+
+    loadView('listings/edit', [
       'listing' => $listing
     ]);
   }
